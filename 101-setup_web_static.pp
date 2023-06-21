def5c7c85e5f9d45fade3b8e506b75e31c223bf2
touchemma <emmagithub1@gmail.com>
2023-06-21 03:14:09 -0700

    all task

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -1,87 +1,63 @@
-# Puppet for setup
-
-$nginx_conf = "server {
-    listen 80 default_server;
-    listen [::]:80 default_server;
-    add_header X-Served-By ${hostname};
-    root   /var/www/html;
-    index  index.html index.htm;
-    location /hbnb_static {
-        alias /data/web_static/current;
-        index index.html index.htm;
-    }
-    location /redirect_me {
-        return 301 http://linktr.ee/firdaus_h_salim/;
-    }
-    error_page 404 /404.html;
-    location /404 {
-      root /var/www/html;
-      internal;
-    }
-}"
-
-package { 'nginx':
-  ensure   => 'present',
-  provider => 'apt'
+# Setup the web servers for the deployment of web_static
+exec { '/usr/bin/env apt -y update' : }
+-> package { 'nginx':
+  ensure => installed,
 }
-
 -> file { '/data':
   ensure  => 'directory'
 }
-
 -> file { '/data/web_static':
   ensure => 'directory'
 }
-
 -> file { '/data/web_static/releases':
   ensure => 'directory'
 }
-
 -> file { '/data/web_static/releases/test':
   ensure => 'directory'
 }
-
 -> file { '/data/web_static/shared':
   ensure => 'directory'
 }
-
 -> file { '/data/web_static/releases/test/index.html':
   ensure  => 'present',
-  content => "this webpage is found in data/web_static/releases/test/index.htm \n"
+  content => "<!DOCTYPE html>
+<html>
+  <head>
+  </head>
+  <body>
+    <p>Nginx server test</p>
+  </body>
+</html>"
 }
-
 -> file { '/data/web_static/current':
   ensure => 'link',
   target => '/data/web_static/releases/test'
 }
-
 -> exec { 'chown -R ubuntu:ubuntu /data/':
   path => '/usr/bin/:/usr/local/bin/:/bin/'
 }
-
-file { '/var/www':
+-> file { '/var/www':
   ensure => 'directory'
 }
-
 -> file { '/var/www/html':
   ensure => 'directory'
 }
-
 -> file { '/var/www/html/index.html':
   ensure  => 'present',
-  content => "This is my first upload  in /var/www/index.html***\n"
+  content => "<!DOCTYPE html>
+<html>
+  <head>
+  </head>
+  <body>
+    <p>Nginx server test</p>
+  </body>
+</html>"
 }
-
--> file { '/var/www/html/404.html':
-  ensure  => 'present',
-  content => "Ceci n'est pas une page - Error page\n"
+exec { 'nginx_conf':
+  environment => ['data=\ \tlocation /hbnb_static {\n\t\talias /data/web_static/current;\n\t}\n'],
+  command     => 'sed -i "39i $data" /etc/nginx/sites-enabled/default',
+  path        => '/usr/bin:/usr/sbin:/bin:/usr/local/bin'
 }
-
--> file { '/etc/nginx/sites-available/default':
-  ensure  => 'present',
-  content => $nginx_conf
-}
-
--> exec { 'nginx restart':
-  path => '/etc/init.d/'
+-> service { 'nginx':
+  ensure => running,
 }
